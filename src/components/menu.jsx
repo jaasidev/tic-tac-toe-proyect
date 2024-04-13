1906994652771e9f5dcd0f7c3f2efb6362d6487c
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-13 18:54:11 -0400

    haciendo el boton de limpiar el menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,13 +1,20 @@
-import { Deshacer } from './Icons'
+import { Clean, Deshacer } from './Icons'
 
 export function Menu ({ actions }) {
+  const { handleCharge, restart } = actions
   const handleclick = () => {
-    actions()
+    handleCharge()
+  }
+  const handleclean = () => {
+    restart()
   }
   return (
     <header className='flex items-center justify-center flex-col sm:flex-row sm:justify-between px-3 my-1'>
       <h2 className='text-[3rem] text-purple-600'>Js-TicTac</h2>
-      <button className='p-4 rounded-full border-[0.2rem] border-orange-500' onClick={handleclick}> <Deshacer /></button>
+      <div className='flex gap-3 items-center'>
+        <button className='p-4 rounded-full border-[0.2rem] border-orange-500' onClick={handleclick}> <Deshacer /></button>
+        <button className='p-4 rounded-full border-[0.2rem] border-red-500' onClick={handleclean}> <Clean /></button>
+      </div>
     </header>
   )
 }
--- src/components/tablero.jsx
+++ src/components/tablero.jsx
@@ -8,7 +8,7 @@ export function Tablero () {
   const { board, handleclick, turn, handleCharge, winner, restart } = useBoard()
   return (
     <>
-      <Menu actions={handleCharge} />
+      <Menu actions={{ handleCharge, restart }} />
       <ul className='grid grid-cols-3 grid-rows-3 gap-2 p-2 self-center'>
         {board.map((value, index) => {
           return (
